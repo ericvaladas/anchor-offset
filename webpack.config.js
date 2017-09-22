9af45677c60234289a883bfe950dd58a25071f27
Eric Valadas <ericvaladas@gmail.com>
2017-09-22 01:06:08 -0700

    Config webpack for node and web targets

--- webpack.config.js
+++ webpack.config.js
@@ -4,38 +4,56 @@ const manifest = require('./package.json');
 
 const mainFile = manifest.main;
 const packageName = manifest.name;
-const destinationDirectory = path.dirname(mainFile);
-const exportFileName = path.basename(mainFile, path.extname(mainFile));
 
-module.exports = {
+const webpackModule = {
+  rules: [{
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: ['babel-loader']
+  }]
+};
+
+const webpackPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.LoaderOptionsPlugin({
+    minimize: true,
+    debug: false
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: false,
+    mangle: false,
+    comments: false
+  })
+];
+
+const nodeConfig = {
+  target: 'node',
   entry: ['./src/index.js'],
   output: {
-    path: path.resolve(__dirname, destinationDirectory),
-    filename: `${exportFileName}.js`,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'lib.node.js',
     library: packageName,
     libraryTarget: 'umd'
   },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {loader: 'babel-loader'}
-    }]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: false,
-      mangle: false,
-      comments: false
-    })
-  ]
+  module: webpackModule,
+  plugins: webpackPlugins
+
 };
+
+const webConfig = {
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'lib.js',
+    library: "anchorOffset",
+    libraryExport: "default"
+  },
+  module: webpackModule,
+  plugins: webpackPlugins
+};
+
+module.exports = [nodeConfig, webConfig];
